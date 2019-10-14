f16474fbdb738ed73f2964304dbf3f29074f24a3
wizvee <jeonghye.dev@gmail.com>
2019-10-14 11:33:21 +0900

    Updates

---
+++ precache-manifest.f4b5ceb6b36c0215acc1906dfab7b6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ef14bb2794167415131d15edc01d06",
+    "url": "/news-viewer/index.html"
+  },
+  {
+    "revision": "8ef0418fd181045b8c7b",
+    "url": "/news-viewer/static/css/main.9c44794e.chunk.css"
+  },
+  {
+    "revision": "05c4d45957afa42790ff",
+    "url": "/news-viewer/static/js/2.325a9fee.chunk.js"
+  },
+  {
+    "revision": "8ef0418fd181045b8c7b",
+    "url": "/news-viewer/static/js/main.8603ec7f.chunk.js"
+  },
+  {
+    "revision": "71c5354454481433242f",
+    "url": "/news-viewer/static/js/runtime-main.0f376bb4.js"
+  }
+]);
